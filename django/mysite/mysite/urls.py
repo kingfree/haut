778e092c2885fac4d0eb379d382f7816ae7a96f5
Kingfree <anytjf@gmail.com>
2013-10-13 23:33:54 +0800

    hello, world

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -1,12 +1,6 @@
-from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls import *
+from mysite.views import *
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    ('^hello/$', hello),
 )
