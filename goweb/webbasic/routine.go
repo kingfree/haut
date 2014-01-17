085fc9136596799a9cea9848809de899b602e3ba
kingfree <anytjf@gmail.com>
2014-01-17 14:04:20 +0800

    GO Web 基础

---
+++ goweb/webbasic/routine.go
@@ -0,0 +1,25 @@
+package main
+import (
+    . "fmt"
+    "net/http"
+)
+
+type MyMux struct {}
+
+func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path == "/" {
+        sayhelloName(w, r)
+        return
+    }
+    http.NotFound(w, r)
+    return
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+    Fprintf(w, "hello, world")
+}
+
+func main() {
+    mux := &MyMux{}
+    http.ListenAndServe(":9090", mux)
+}
