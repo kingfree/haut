41f5ad062a3bf79d995a56fd3804e3a642315dbd
kingfree <anytjf@gmail.com>
2014-01-18 17:08:17 +0800

    正则表达式

--- goweb/text/grep.go
+++ goweb/text/grep.go
@@ -9,6 +9,8 @@ import (
 )
 
 func main() {
+	Println("网页抓取")
+
 	resp, err := http.Get("http://cojs.tk/cogs/")
 	if err != nil {
 		Println(err)
@@ -37,4 +39,74 @@ func main() {
 	src = re.ReplaceAllString(src, "\n")
 
 	Println(strings.TrimSpace(src))
+
+	正则应用()
+
+	展开()
+}
+
+func 展开() {
+	Println("展开")
+	src := []byte(`
+		call hello azusa
+		hello kyouko
+		call hello momo
+	`)
+	pat := regexp.MustCompile(`(?m)(call)\s+(?P<cmd>\w+)\s+(?P<arg>.+)\s*$`)
+	res := []byte{}
+	for _, s := range pat.FindAllSubmatchIndex(src, -1) {
+		res = pat.Expand(res, []byte("$cmd('$arg')\n"), src, s)
+	}
+	Println(string(res))
+}
+
+func 正则应用() {
+	Println("正则应用")
+
+	a := "I am learing Go language"
+	Println(a)
+
+	re, _ := regexp.Compile("[a-z]{2,4}")
+
+	one := re.Find([]byte(a))
+	Println("Find:", string(one))
+
+	all := re.FindAll([]byte(a), -1)
+	Print("FindAll:")
+	for _, v := range all {
+		Print(" [", string(v), "]")
+	}
+	Println()
+
+	index := re.FindIndex([]byte(a))
+	Println("FindIndex:", index)
+
+	allindex := re.FindAllIndex([]byte(a), -1)
+	Println("FindAllIndex:", allindex)
+
+	rf, _ := regexp.Compile("am(.*)lang(.*)")
+
+	submatch := rf.FindSubmatch([]byte(a))
+	Print("FindSubmatch:")
+	for _, v := range submatch {
+		Print(" [", string(v), "]")
+	}
+	Println()
+
+	submatchindex := rf.FindSubmatchIndex([]byte(a))
+	Println("FindSubmatchIndex:", submatchindex)
+
+	submatchall := rf.FindAllSubmatch([]byte(a), -1)
+	Print("FindAllSubmatch:")
+	for _, w := range submatchall {
+		Print(" [")
+		for _, v := range w {
+			Print("[", string(v), "] ")
+		}
+		Print("] ")
+	}
+	Println()
+
+	submatchallindex := rf.FindAllSubmatchIndex([]byte(a), -1)
+	Println("FindAllStringSubmatchIndex:", submatchallindex)
 }
