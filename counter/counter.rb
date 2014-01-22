ef531c30201aed873ee8c69028fcef929873f9a1
kingfree <anytjf@gmail.com>
2014-01-22 23:00:39 +0800

    添加注释

--- counter/counter.rb
+++ counter/counter.rb
@@ -34,7 +34,7 @@ class Contest
     sort!
     printf "%s\n", @title
     # puts "运营：#{@lz}"
-    # printf "开始时间：%s  结束时间：%s\n", @time_b.strftime(TIMEFMT), @time_e.strftime(TIMEFMT)
+    printf "开始时间：%s  结束时间：%s\n", @time_b.strftime(TIMEFMT), @time_e.strftime(TIMEFMT)
     # puts "单人投票限制：#{@vote_limit}票"
     printf "当前时间：%s  有效票数：%d票\n", @ima.strftime(TIMEFMT), @nums
     @groups.each do |e|
@@ -62,16 +62,19 @@ class Contest
   end
 
   def find(name, grp = "")
+    # 不指定无效票认为是有效票，查找之
+    # TODO: 顺序查找效率较低，需要改善算法
     @groups.each_index do |i|
       next if @groups[i].name == MUKOU and grp == ""
       res = @groups[i].charas.find_index { |f| f.name == name }
-      return i, res if res
+      return i, res if res # 是有效票则返回
     end
-    return add_mukou(name) if grp == MUKOU
+    return add_mukou(name) if grp == MUKOU # 添加一个无效票
     return nil, nil
   end
 
   def add_mukou(name)
+    # 添加新的无效票
     i = @groups.find_index { |x| x.name == MUKOU }
     n = Character.new(name)
     @groups[i].charas.push(n)
@@ -79,9 +82,9 @@ class Contest
   end
 
   def add(name, grp = "")
-    a, b = find(name, grp)
-    return false if a == nil and b == nil
-    @groups[a].charas[b].add
+    a, b = find(name, grp) # 尝试查找
+    return false if a == nil and b == nil # 找不到返回假
+    @groups[a].charas[b].add # 找到了，计数
     return true
   end
 end
@@ -131,7 +134,8 @@ class Post
     @aid = aid.to_i
     @author = author
     @floor = floor.to_i
-    @date = date
+    l = date.scanf("%d-%d-%d %d:%d")
+    @date = Time.new(l[0], l[1], l[2], l[3], l[4], 00)
     @text = text
     @votes = Array.new
   end
@@ -157,25 +161,30 @@ class Posts
   end
 
   def mukou(t)
+    return true if t.date > @comp.time_e or t.date < @comp.time_b
+    # 超过比赛时间
+    return true if @posts.find_index { |x| x.aid == t.aid }
+    # 已经投过票
     return false
+    # TODO: 需要添加白名单和黑名单过滤
   end
 
   def tryadd(votes)
     i = 0
     votes.each do |m|
-      return i if comp.add(m)
+      return i if comp.add(m) # 返回第一个合法值的下标
       i += 1
     end
     return nil
   end
 
   def count(t, tpl)
-    t.text.scan(tpl) { |m| t.votes.push(m.to_s) }
-    unless mukou(t)
-      i = tryadd(t.votes)
-      t.votes.delete_at(i) if i != nil
+    t.text.scan(tpl) { |m| t.votes.push(m.to_s) } # 把票放进数组里
+    unless mukou(t) # 如果投票者合法
+      i = tryadd(t.votes) # 尝试把投票记录下来
+      t.votes.delete_at(i) if i != nil # 如果投票合法就删掉
     end
-    t.votes.each { |m| @comp.add(m, MUKOU) }
+    t.votes.each { |m| @comp.add(m, MUKOU) } # 剩下的全是无效票
   end
 
   def parse(page)
@@ -190,9 +199,9 @@ class Posts
       )
       @posts.push(t)
       if t.floor == 1
-        @comp.lz = t.author
+        @comp.lz = t.author # 运营
       end
-      if t.author == @comp.lz
+      if t.author == @comp.lz # 运营发布的消息
         if @comp.time_b == nil
           @comp.settime!(
             t.text.match(/开始时间：([0-9]+月[0-9]+日[0-9]+:[0-9]{,2})/)[1],
@@ -200,15 +209,16 @@ class Posts
           )
         end
         if t.text.match('出场阵容')
-          g = Group.new(MUKOU)
+          g = Group.new(MUKOU) # 无效票组
           t.text.scan(/(<<.+?>>|[^><]+?（.+?场）)/) do |uu|
             u = uu[0].to_s
-            if u.match('场')
+            if u.match('<<')
+              k = Character.new(u)
+              g.addcharas(k) # 规定的有效票
+            elsif u.match('场）')
+              # 如果是“场”单字会匹配“战场原黑仪”，稳妥起见放在第二分支
               @comp.addgroup(g)
               g = Group.new(u)
-            elsif u.match('<<')
-              k = Character.new(u)
-              g.addcharas(k)
             end
           end
           @comp.addgroup(g)
@@ -240,6 +250,7 @@ class Posts
       $stderr.print "[#{pn}]"
       page = Nokogiri::HTML(open("#{url}?pn=#{pn}"))
       parse(page)
+      # TODO: 有必要缓存页面或解析结果的前 lastpn - 1 页内容
     end
     $stderr.puts " 完成"
   end
@@ -252,10 +263,20 @@ if $0 == __FILE__
   while line = $stdin.gets.to_i and line > 0
     pid.push(line)
   end
-  puts "还没有统计白名单和黑名单，结果仅供参考"
   pid.each do |p|
     tie = Posts.new(max)
     tie.fetch(p)
     tie.comp.output
+    # tie.output
   end
 end
+
+# 应该实现一个高并发的Go的服务器，然后在网页用Ajax查询Go服务器
+# Ruby解释器定时翻新最后一页，并把数据返回给Go服务器，减轻负荷
+#       HTML/Ajax Page <=> Go Server <=> Ruby Parser
+#
+# 功能自定义性强，去伪/无效/萌文/曲线图/...功能给出接口可选
+# 将来要适应联赛/歌赏/新星萌/日萌……
+#
+# 也应该实现一个图形界面，用C/C++/Qt重写，也考虑C#/.Net方案
+# 必须发布成可执行文件
