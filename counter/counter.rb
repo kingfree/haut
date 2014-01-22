3f83c40ce9c0559aaea1b4cbc66b5a73551c0f63
kingfree <anytjf@gmail.com>
2014-01-22 21:38:02 +0800

    限制单次投票数量，记录为无效票

--- counter/counter.rb
+++ counter/counter.rb
@@ -2,35 +2,47 @@ require 'rubygems'
 require 'nokogiri'
 require "open-uri"
 require "json"
+require "scanf"
+
+TIMEFMT = "%Y年%m月%d日 %H:%M:%S"
+MUKOU = "无效"
 
 class Contest
   attr_accessor :title, :time_b, :time_e, :vote_limit, :lz, :groups, :nums, :ima
 
   def initialize()
     @title = ""
-    @time_b = ""
-    @time_e = ""
     @vote_limit = 1
     @lz = ""
+    @time_b = nil
+    @time_e = nil
     @nums = 0
     @groups = Array.new
     @ima = Time.now
+    @ghash = Hash.new
+  end
+
+  def settime!(s, t)
+    year = Time.now.year
+    l = s.scanf("%d月%d日%d:%d")
+    @time_b = Time.new(year, l[0], l[1], l[2], l[3], 00)
+    l = t.scanf("%d月%d日%d:%d")
+    @time_e = Time.new(year, l[0], l[1], l[2], l[3], 59)
   end
 
   def output
     sort!
-    puts "#{@title}"
+    printf "%s\n", @title
     # puts "运营：#{@lz}"
-    # puts "开始时间：#{@time_b}  结束时间：#{@time_e}"
+    # printf "开始时间：%s  结束时间：%s\n", @time_b.strftime(TIMEFMT), @time_e.strftime(TIMEFMT)
     # puts "单人投票限制：#{@vote_limit}票"
-    puts "当前时间：#{@ima} 投票总数：#{@nums}票"
+    printf "当前时间：%s  有效票数：%d票\n", @ima.strftime(TIMEFMT), @nums
     @groups.each do |e|
-      puts ""
-      puts "#{e.name} 共#{e.nums}票"
+      printf "\n%s 共%d票\n", e.name, e.nums
       i = 0
       e.charas.each do |f|
         i += 1
-        puts "#{i}位 #{f.name} #{f.nums}票"
+        printf "%2d位 %3d票 %s\n", i, f.nums, f.name
       end
     end
   end
@@ -39,7 +51,8 @@ class Contest
     @nums = 0
     @groups.each do |e|
       e.sort!
-      @nums += e.count!
+      e.count!
+      @nums += e.nums if e.name != MUKOU
     end
     @groups.sort_by! { |x| - x.nums }
   end
@@ -48,20 +61,28 @@ class Contest
     @groups.push(group)
   end
 
-  def find(name)
+  def find(name, grp = "")
     @groups.each_index do |i|
+      next if @groups[i].name == MUKOU and grp == ""
       res = @groups[i].charas.find_index { |f| f.name == name }
       return i, res if res
     end
-    i = @groups.find_index { |f| f.name == "无效" }
+    return add_mukou(name) if grp == MUKOU
+    return nil, nil
+  end
+
+  def add_mukou(name)
+    i = @groups.find_index { |x| x.name == MUKOU }
     n = Character.new(name)
     @groups[i].charas.push(n)
     return i, @groups[i].charas.length - 1
   end
 
-  def add(name)
-    a, b = find(name)
+  def add(name, grp = "")
+    a, b = find(name, grp)
+    return false if a == nil and b == nil
     @groups[a].charas[b].add
+    return true
   end
 end
 
@@ -103,21 +124,8 @@ class Character
   end
 end
 
-class Counter
-  def initialize()
-    @counter = Hash.new
-    @black_name = Array.new
-  end
-
-  def addblack(name)
-    return if @black_name.include?(t.author)
-    name = name.to_s
-    @black_name.push(name) if not @black_name.include?(name)
-  end
-end
-
 class Post
-  attr_accessor :aid, :author, :floor, :date, :text
+  attr_accessor :aid, :author, :floor, :date, :text, :votes
 
   def initialize(aid, author, floor, date, text)
     @aid = aid.to_i
@@ -125,34 +133,49 @@ class Post
     @floor = floor.to_i
     @date = date
     @text = text
+    @votes = Array.new
   end
 
   def output
     puts "#{@author}(#{@aid}) #{@floor}楼 #{@date}"
     puts "  #{@text}"
   end
+
 end
 
 class Posts
-  attr_accessor :posts, :cnt, :comp
+  attr_accessor :posts, :comp
 
   def initialize(limit = 0)
     @posts = Array.new
-    @cnt = Counter.new
     @limit = limit.to_i
     @comp = Contest.new
   end
 
   def output()
-    puts @blacklist
     @posts.each { |t| t.output }
   end
 
-  def count(t, tpl)
-    t.text.scan(tpl) do |m|
-      name = m.to_s
-      @comp.add(name)
+  def mukou(t)
+    return false
+  end
+
+  def tryadd(votes)
+    i = 0
+    votes.each do |m|
+      return i if comp.add(m)
+      i += 1
     end
+    return nil
+  end
+
+  def count(t, tpl)
+    t.text.scan(tpl) { |m| t.votes.push(m.to_s) }
+    unless mukou(t)
+      i = tryadd(t.votes)
+      t.votes.delete_at(i) if i != nil
+    end
+    t.votes.each { |m| @comp.add(m, MUKOU) }
   end
 
   def parse(page)
@@ -167,26 +190,28 @@ class Posts
       )
       @posts.push(t)
       if t.floor == 1
-        comp.lz = t.author
+        @comp.lz = t.author
       end
-      if t.author == comp.lz
-        if comp.time_b == "" and comp.time_e == ""
-          comp.time_b = t.text.match(/开始时间：([0-9]+月[0-9]+日[0-9]+:[0-9]{,2})/)[1]
-          comp.time_e = t.text.match(/结束时间：([0-9]+月[0-9]+日[0-9]+:[0-9]{,2})/)[1]
+      if t.author == @comp.lz
+        if @comp.time_b == nil
+          @comp.settime!(
+            t.text.match(/开始时间：([0-9]+月[0-9]+日[0-9]+:[0-9]{,2})/)[1],
+            t.text.match(/结束时间：([0-9]+月[0-9]+日[0-9]+:[0-9]{,2})/)[1]
+          )
         end
         if t.text.match('出场阵容')
-          g = Group.new("无效")
+          g = Group.new(MUKOU)
           t.text.scan(/(<<.+?>>|[^><]+?（.+?场）)/) do |uu|
             u = uu[0].to_s
             if u.match('场')
-              comp.addgroup(g)
+              @comp.addgroup(g)
               g = Group.new(u)
             elsif u.match('<<')
               k = Character.new(u)
               g.addcharas(k)
             end
           end
-          comp.addgroup(g)
+          @comp.addgroup(g)
         end
       else
         count(t, /<<.+?>>/)
@@ -201,7 +226,7 @@ class Posts
     $stderr.puts url
     page = Nokogiri::HTML(open(url))
 
-    comp.title = page.css('.core_title_txt')[0].text
+    comp.title = page.css('.core_title_txt')[0].attr('title')
 
     pager = page.css('.l_pager')[0]
     pages = pager.css('a').select { |link| link.text == "尾页" }
@@ -222,8 +247,12 @@ end
 
 if $0 == __FILE__
   max = ARGV[0].to_i
-  # puts "输入帖子编号："
-  pid = [2826568816, 2826568427, 2826569611]
+  $stderr.puts "输入帖子编号(负数或0结束)："
+  pid = Array.new
+  while line = $stdin.gets.to_i and line > 0
+    pid.push(line)
+  end
+  puts "还没有统计白名单和黑名单，结果仅供参考"
   pid.each do |p|
     tie = Posts.new(max)
     tie.fetch(p)
