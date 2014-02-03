f3d82c795c0e6f5557399669b2e1082d41c7d12a
kingfree <anytjf@gmail.com>
2014-02-03 14:27:55 +0800

    HashFold 3

--- ruby/hf.rb
+++ ruby/hf.rb
@@ -16,7 +16,7 @@ class HashFold # 单线程
   end
 end
 
-class HashFold_m # 多线程, Linux only
+class HashFold1 # 多线程 1, Linux only
   def hash_merge(hash, k, v)
     if hash.key?(k)
       hash[k] = self.fold(hash[k], v)
@@ -50,6 +50,99 @@ class HashFold_m # 多线程, Linux only
       end
     end
     hash
+  end
+end
+
+class HashFold2
+  class Pool
+    def initalize(hf, n)
+      pool = n.times.map {
+        c0, p0 = IO.pipe
+        p1, c1 = IO.pipe
+        fork do
+          p0.close
+          p1.close
+          loop do
+            input = Marshal.load(c0) rescue exit
+            hash = {}
+            hf.map(input) do |k, v|
+              hf.hash_merge(hash, k, v)
+            end
+            Marshal.dump(hash, c1)
+          end
+        end
+        c0.close
+        c1.close
+        [p0, p1]
+      }
+      @inputs = pool.map{|i, o| i}
+      @outputs = pool.map{|i, o| o}
+      @ichann = @inputs.dup
+      @queue = []
+      @results = []
+    end
+
+    def flush
+      loop do
+        if @ichann.empty?
+          o, @ichann, e = IO.select([], @inputs, [])
+          break if @ichann.empty?
+        end
+        break if @queue.empty?
+        Marshal.dump(@queue.pop, @ichann.pop)
+      end
+    end
+    private :flush
+
+    def push(obj)
+      @queue.push obj
+      flush
+    end
+
+    def fill
+      t = @results.size == 0 ? nil : 0
+      ochann, i, e = IO.select(@outputs, [], [], t)
+      return if ochann == nil
+      ochann.each do
+        c = ochann.pop
+        begin
+          @results.push Marshal.load(c)
+        rescue => e
+          c.close
+          @outputs.delete(c)
+        end
+      end
+    end
+    private :fill
+
+    def result
+      fill
+      @results.pop
+    end
+  end
+
+  def initalize(n = 2)
+    @pool = Pool.new(self, n)
+  end
+
+  def hash_merge(hash, k, v)
+    if hash.key?(k)
+      hash[k] = self.fold(hash[k], v)
+    else
+      hash[k] = v
+    end
+  end
+
+  def start(inputs)
+    inputs.each do |input|
+      @pool.push input
+    end
+    hash = {}
+    inputs.each do |input|
+      @pool.result.each do |k, v|
+        hash_merge(hash, k, v)
+      end
+    end
   end
 end
 
