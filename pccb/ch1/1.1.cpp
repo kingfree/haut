dd0652a74bf3b24e65e94d512130cd56fe3794a4
Kingfree <anytjf@gmail.com>
2014-02-23 21:11:19 +0800

    1.1

--- pccb/ch1/1.1.cpp
+++ pccb/ch1/1.1.cpp
@@ -1,32 +1,41 @@
 /* 抽签
 判断是否存在k[1..n]中取出四个数（有放回）之和为m
+1<=n<=50, 1<=m<=1e8, 1<=k[i]<=1e8
 */
 #include <cstdio>
+
 const int N = 50;
+// 全局变量保存数据
+int n, m, k[N];
+
+// 只解决问题而不输入的解题函数
+void solve() {
+  // 是否找到和为m的组合的标记
+  bool f = false;
+  // 通过四重循环枚举所有方案
+  for(int a = 0; a < n; a++) {
+    for(int b = 0; b < n; b++) {
+      for(int c = 0; c < n; c++) {
+        for(int d = 0; d < n; d++) {
+          if(k[a] + k[b] + k[c] + k[d] == m) {
+            f = true;
+          }
+        }
+      }
+    }
+  }
+  // 输出到标准输出
+  puts(f ? "Yes" : "No");
+}
+
 int main() {
-  int n, m, k[N];
   // 读入多组数据
   while(scanf("%d%d", &n, &m) > 0) {
     // 从标准输入读入
     for(int i = 0; i < n; i++) {
       scanf("%d", &k[i]);
     }
-    // 是否找到和为m的组合的标记
-    bool f = false;
-    // 通过四重循环枚举所有方案
-    for(int a = 0; a < n; a++) {
-      for(int b = 0; b < n; b++) {
-        for(int c = 0; c < n; c++) {
-          for(int d = 0; d < n; d++) {
-            if(k[a] + k[b] + k[c] + k[d] == m) {
-              f = true;
-            }
-          }
-        }
-      }
-    }
-    // 输出到标准输出
-    puts(f ? "Yes" : "No");
+    solve();
   }
   return 0;
 }
