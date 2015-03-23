dc6f4701324d09069a1cc80e7f90d7ec26d37f3d
Kingfree <anytjf@gmail.com>
2015-03-23 21:20:26 +0800

    使用栈实现计数

--- java/course/exp4/src/dir/Dir.java
+++ java/course/exp4/src/dir/Dir.java
@@ -7,6 +7,7 @@ import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.*;
 import java.text.*;
+import java.util.Stack;
 
 /**
  * @date 2015-3-22
@@ -14,51 +15,52 @@ import java.text.*;
  */
 public class Dir {
 
-    public static String humanSize(long size) {
-        if (size <= 0)
-            return "0";
-        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
-        int n = (int) (Math.log10(size) / Math.log10(1024));
-        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, n))
-                + " " + units[n];
-    }
+	public static String humanSize(long size) {
+		if (size <= 0)
+			return "0";
+		final String[] units = new String[] { "B", "k", "M", "G", "T" };
+		int n = (int) (Math.log10(size) / Math.log10(1024));
+		return new DecimalFormat("#0.#").format(size / Math.pow(1024, n))
+				+ units[n];
+	}
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) throws IOException {
-        String path = args.length < 1 ? ".." : args[0];
-        Path dir = Paths.get(path);
-        Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		String path = args.length < 1 ? "." : args[0];
+		Path dir = Paths.get(path);
+		Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
 
-            private long fileSize = 0;
+			private Stack<Long> fileSizes = new Stack<Long>();
 
-            public FileVisitResult visitFile(Path file,
-                    BasicFileAttributes attrs) throws IOException {
-                fileSize += attrs.size();
-                System.out.printf("      %8s %s\n", humanSize(attrs.size()),
-                        file.normalize());
-                return FileVisitResult.CONTINUE;
-            }
+			public FileVisitResult visitFile(Path file,
+					BasicFileAttributes attrs) throws IOException {
+				fileSizes.push(fileSizes.pop() + attrs.size());
+				System.out.printf(" %6s  %s\n", humanSize(attrs.size()), file.normalize());
+				return FileVisitResult.CONTINUE;
+			}
 
-            public FileVisitResult preVisitDirectory(Path dir,
-                    BasicFileAttributes attrs) throws IOException {
-                fileSize = 0;
-                return FileVisitResult.CONTINUE;
-            }
+			public FileVisitResult preVisitDirectory(Path dir,
+					BasicFileAttributes attrs) throws IOException {
+				fileSizes.push(Long.valueOf(0));
+				return FileVisitResult.CONTINUE;
+			}
 
-            public FileVisitResult postVisitDirectory(Path dir, IOException exc)
-                    throws IOException {
-                System.out.printf("<DIR> %8s %s\n", humanSize(fileSize),
-                        dir.normalize());
-                return FileVisitResult.CONTINUE;
-            }
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc)
+					throws IOException {
+				long size = fileSizes.pop();
+				System.out.printf("[%6s] %s\n", humanSize(size), dir.normalize());
+				if (!fileSizes.isEmpty())
+					fileSizes.push(fileSizes.pop() + size);
+				return FileVisitResult.CONTINUE;
+			}
 
-            public FileVisitResult visitFileFailed(Path file, IOException exc)
-                    throws IOException {
-                return FileVisitResult.CONTINUE;
-            }
-        });
-    }
+			public FileVisitResult visitFileFailed(Path file, IOException exc)
+					throws IOException {
+				return FileVisitResult.CONTINUE;
+			}
+		});
+	}
 
 }
