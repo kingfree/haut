8fd65d7fad2a659b95e74278b8df8d762d308230
Kingfree <anytjf@live.com>
2015-04-03 19:54:33 +0800

    rand

---
+++ java/course/ch4/src/prob15/RandomCompare.java
@@ -0,0 +1,29 @@
+package prob15;
+
+import java.util.Random;
+
+/**
+ * @version 2015-4-3
+ * @author Kingfree
+ */
+public class RandomCompare {
+
+	public static void main(String[] args) {
+		int n = 10;
+		int[] a = new int[n];
+		Random rand = new Random();
+		for (int i = 0; i < n; i++) {
+			a[i] = rand.nextInt(1000);
+		}
+		for (int e : a) {
+			System.out.print(e + " ");
+		}
+		System.out.println();
+
+		for (int i = 0; i < n - 1; i++) {
+			char c = a[i] < a[i + 1] ? '<' : ((a[i] == a[i + 1]) ? '=' : '>');
+			System.out.println(a[i] + " " + c + " " + a[i + 1]);
+		}
+	}
+
+}
