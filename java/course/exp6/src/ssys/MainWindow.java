d6ae2a7d979ba15489332b2fd959e3429afde30d
Kingfree <anytjf@live.com>
2015-04-04 20:24:47 +0800

    学生类

--- java/course/exp6/src/ssys/MainWindow.java
+++ java/course/exp6/src/ssys/MainWindow.java
@@ -2,12 +2,15 @@ package ssys;
 
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.util.Enumeration;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
 
 /**
  * 主窗口
@@ -16,25 +19,43 @@ import javax.swing.JScrollPane;
  * @author Kingfree
  */
 public class MainWindow {
-	MainWindow() {
-		JFrame frame = new JFrame("学生信息管理系统");
-		frame.setSize(800, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new FlowLayout());
+    private static void InitGlobalFont(Font font) {
+        FontUIResource fontRes = new FontUIResource(font);
+        for (Enumeration<Object> keys = UIManager.getDefaults().keys(); keys
+                .hasMoreElements();) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get(key);
+            if (value instanceof FontUIResource) {
+                UIManager.put(key, fontRes);
+            }
+        }
+    }
 
-		JLabel label = new JLabel("班级：");
-		frame.add(label);
+    MainWindow() {
 
-		String[] classes = { "软件1301班", "软件1302班", "软件1303班", "软件1304班",
-				"软件1305班" };
-		JComboBox<String> combobox = new JComboBox<String>(classes);
-		frame.add(combobox);
-		
-		JList<String> list = new JList<>(classes);
-		JScrollPane listPane = new JScrollPane(list); 
-		frame.add(listPane);
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            InitGlobalFont(new Font("微软雅黑", Font.PLAIN, 12));
+        } catch (Exception e) {
+        }
 
-		WindowFeature.modifyWindow(frame, new Font("微软雅黑", Font.PLAIN, 12));
-		frame.setVisible(true);
-	}
+        JFrame frame = new JFrame("学生信息管理系统");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        JLabel label = new JLabel("班级：");
+        frame.add(label);
+
+        String[] classes = { "软件1301班", "软件1302班", "软件1303班", "软件1304班",
+                "软件1305班" };
+        JComboBox<String> combobox = new JComboBox<String>(classes);
+        frame.add(combobox);
+
+        JList<String> list = new JList<>(classes);
+        JScrollPane listPane = new JScrollPane(list);
+        frame.add(listPane);
+
+        frame.setVisible(true);
+    }
 }
