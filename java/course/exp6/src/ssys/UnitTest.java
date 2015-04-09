53a5844f8f37853e56ece0953ae2f086f4abff7f
Kingfree <anytjf@gmail.com>
2015-04-09 19:57:44 +0800

    创建SQL

--- java/course/exp6/src/ssys/UnitTest.java
+++ java/course/exp6/src/ssys/UnitTest.java
@@ -8,56 +8,69 @@ import java.sql.*;
  */
 public class UnitTest {
 
-	/**
-	 * @param args
-	 * @throws ClassNotFoundException
-	 */
-	public static void main(String[] args) throws ClassNotFoundException {
-		// load the sqlite-JDBC driver using the current class loader
-		Class.forName("org.sqlite.JDBC");
+    /**
+     * @param args
+     * @throws ClassNotFoundException
+     */
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
 
-		Connection connection = null;
-		try {
-			// create a database connection
-			connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-			Statement statement = connection.createStatement();
-			statement.setQueryTimeout(30); // set timeout to 30 sec.
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            Statement sql = connection.createStatement();
+            sql.setQueryTimeout(30);
 
-			statement.executeUpdate("drop table if exists students");
-			statement
-					.executeUpdate("create table students (id integer, name string, "
-							+ "math integer, os integer, java integer)");
-			statement
-					.executeUpdate("insert into students values(12, '张三', 90, 90, 90)");
-			ResultSet rs = statement.executeQuery("select * from students");
-			while (rs.next()) {
-				System.out.printf("%s %d os:%d java:%d math:%d",
-						rs.getString("name"), rs.getInt("id"), rs.getInt("os"),
-						rs.getInt("java"), rs.getInt("math"));
-			}
-		} catch (SQLException e) {
-			// if the error message is "out of memory",
-			// it probably means no database file is found
-			System.err.println(e.getMessage());
-		} finally {
-			try {
-				if (connection != null)
-					connection.close();
-			} catch (SQLException e) {
-				// connection close failed.
-				System.err.println(e);
-			}
-		}
-	}
+            sql.executeUpdate("DROP TABLE IF EXISTS students");
+            sql.executeUpdate("CREATE TABLE students(id INTEGER PRIMARY KEY, name STRING)");
+            sql.executeUpdate("DROP TABLE IF EXISTS subjects");
+            sql.executeUpdate("CREATE TABLE subjects (id INTEGER PRIMARY KEY, name STRING"
+                    + ", memo STRING)");
+            sql.executeUpdate("DROP TABLE IF EXISTS scores");
+            sql.executeUpdate("CREATE TABLE scores(" + "student_id INTEGER"
+                    + ", subject_id INTEGER" + ", score INTEGER"
+                    + ", FOREIGN KEY(student_id) REFERENCES students(id)"
+                    + ", FOREIGN KEY(subject_id) REFERENCES subjects(id)" + ")");
 
-	public UnitTest() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+            sql.executeUpdate("INSERT INTO subjects VALUES(1, 'os', '操作系统')");
+            sql.executeUpdate("INSERT INTO subjects VALUES(2, 'math', '离散数学')");
+            sql.executeUpdate("INSERT INTO subjects VALUES(3, 'java', '程序设计')");
 
-	@Override
-	public String toString() {
-		return "UnitTest []";
-	}
+            sql.executeUpdate("INSERT INTO students VALUES(12, '张三')");
+            sql.executeUpdate("INSERT INTO scores VALUES(12, 1, 90)");
+            sql.executeUpdate("INSERT INTO scores VALUES(12, 2, 90)");
+            sql.executeUpdate("INSERT INTO scores VALUES(12, 3, 90)");
+
+            ResultSet rs = sql.executeQuery("SELECT"
+                    + " students.name, subjects.name as subject_name"
+                    + ", subjects.memo, scores.score"
+                    + " FROM students, subjects, scores"
+                    + " WHERE students.id = scores.student_id"
+                    + " AND subjects.id = scores.subject_id");
+            while (rs.next()) {
+                System.out.printf("%s %s[%s]:%d\n", rs.getString("name"),
+                        rs.getString("subject_name"), rs.getString("memo"),
+                        rs.getInt("score"));
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+
+    public UnitTest() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "UnitTest []";
+    }
 
 }
