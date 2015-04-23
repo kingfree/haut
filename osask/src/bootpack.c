8e8e3fc8465652b501581b54c2d57757f19c2460
Kingfree <anytjf@gmail.com>
2015-04-23 19:59:35 +0800

    DAY04 应用指针

--- osask/src/bootpack.c
+++ osask/src/bootpack.c
@@ -1,12 +1,14 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
     int i; /* 声明变量 */
+    char *p; /* 指向BYTE */
 
     for (i = 0xa0000; i <= 0xaffff; i++) {
-        write_mem8(i, i & 0x0f); /* MOV BYTE [i], 15 */
+        p = (char *) i;
+        *p = i & 0x0f;
+        /* 相当于 write_mem8(i, i & 0x0f); */
     }
 
     for (; ; ) {
