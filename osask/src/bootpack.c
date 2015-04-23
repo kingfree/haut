a984e018f6dcc6e545abdd4b4d69ee783e67ba71
Kingfree <anytjf@gmail.com>
2015-04-23 22:01:24 +0800

    DAY04 绘制任务栏

--- osask/src/bootpack.c
+++ osask/src/bootpack.c
@@ -6,55 +6,82 @@ void io_store_eflags(int eflags);
 
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
-void boxfill8(unsigned char *vram, int xsize, unsigned char c,
+void boxfill8(unsigned char *vram, int X, unsigned char c,
               int x0, int y0, int x1, int y1);
+void boxsize8(unsigned char *vram, int X, unsigned char c,
+              int x0, int y0, int width, int height);
 
-#define COL8_BASE03   0
-#define COL8_002B36   0
-#define COL8_BASE02   1
-#define COL8_073642   1
-#define COL8_BASE01   2
-#define COL8_586E75   2
-#define COL8_BASE00   3
-#define COL8_657B83   3
-#define COL8_BASE0    4
-#define COL8_839496   4
-#define COL8_BASE1    5
-#define COL8_93A1A1   5
-#define COL8_BASE2    6
-#define COL8_EEE8D5   6
-#define COL8_BASE3    7
-#define COL8_FDF6E3   7
-#define COL8_YELLOW   8
-#define COL8_B58900   8
-#define COL8_ORANGE   9
-#define COL8_CB4B16   9
-#define COL8_RED     10
-#define COL8_DC322F  10
-#define COL8_MAGENTA 11
-#define COL8_D33682  11
-#define COL8_VIOLET  12
-#define COL8_6C71C4  12
-#define COL8_BLUE    13
-#define COL8_268BD2  13
-#define COL8_CYAN    14
-#define COL8_2AA198  14
-#define COL8_GREEN   15
-#define COL8_859900  15
+#define base03   0
+#define base02   1
+#define base01   2
+#define base00   3
+#define base0    4
+#define base1    5
+#define base2    6
+#define base3    7
+#define yellow   8
+#define orange   9
+#define red     10
+#define magenta 11
+#define violet  12
+#define blue    13
+#define cyan    14
+#define green   15
+
+/* 为了兼容OSASK颜色 */
+#define COL8_000000   0
+#define COL8_FF0000   1
+#define COL8_00FF00   2
+#define COL8_FFFF00   3
+#define COL8_0000FF   4
+#define COL8_FF00FF   5
+#define COL8_00FFFF   6
+#define COL8_FFFFFF   7
+#define COL8_C6C6C6   8
+#define COL8_840000   9
+#define COL8_008400  10
+#define COL8_848400  11
+#define COL8_000084  12
+#define COL8_840084  13
+#define COL8_008484  14
+#define COL8_848484  15
 
 void HariMain(void)
 {
-    char *p;
+    char *vram;
+    int X, Y;
 
-    init_palette(); /* 设定调色板 */
+    init_palette();
+    vram = (char *) 0xa0000;
+    X = 320;
+    Y = 200;
 
-    p = (char *) 0xa0000;
+    /* Windows 徽标大小 */
+    int box = 10, space = 2, top = 3;
+    
+    /* 桌面 */
+    boxfill8(vram, X, cyan , 0, 0, X - 1, Y - 1);
+    
+    /* 任务栏 */
+    int height = box * 2 + space + top * 2 - 1;
+    boxfill8(vram, X, base2 , 0, Y - height, X -  1, Y - 1);
+    boxfill8(vram, X, base3 , 0, Y - height, height - 1, Y - 1);
 
-    boxfill8(p, 320, COL8_MAGENTA,  20,  20, 120, 120);
-    boxfill8(p, 320, COL8_CYAN   ,  70,  50, 170, 150);
-    boxfill8(p, 320, COL8_RED    , 120,  80, 220, 180);
+    /* 开始按钮 */
+    int tops = top + box + space - 1;
+    int botm = top + box;
+    int bots = botm + space + box - 1;
+    boxsize8(vram, X, red    ,  top, Y - bots, box, box);
+    boxsize8(vram, X, green  , tops, Y - bots, box, box);
+    boxsize8(vram, X, blue   ,  top, Y - botm, box, box);
+    boxsize8(vram, X, yellow , tops, Y - botm, box, box);
 
-    for (; ; ) {
+    /* 托盘区*/
+    int right = 100;
+    boxsize8(vram, X, base3 , X - right, Y - height, 2, height);
+    boxsize8(vram, X, base1 , X - right + 1, Y - (height + 8) / 2, 0, 8);
+    
+    for (; ; ) { 
         io_hlt();
     }
 }
@@ -62,41 +89,23 @@ void HariMain(void)
 void init_palette(void)
 {
     static unsigned char table_rgb[16 * 3] = {
-        /* solarized 配色方案
-         * http://ethanschoonover.com/solarized/img/solarized-palette.png
-         */
-        0x00, 0x2b, 0x36, /* base03 */
+        /* 参考但有改动 http://ethanschoonover.com/solarized */
+        0x00, 0x00, 0x00, /* base03 */
         0x07, 0x36, 0x42, /* base02 */
         0x58, 0x6e, 0x75, /* base01 */
         0x65, 0x7b, 0x83, /* base00 */
         0x83, 0x94, 0x96, /* base0 */
         0x93, 0xa1, 0xa1, /* base1 */
         0xee, 0xe8, 0xd5, /* base2 */
-        0xfd, 0xf6, 0xe3, /* base3 */
-        0xb5, 0x89, 0x00, /* yellow */
+        0xff, 0xff, 0xff, /* base3 */
+        0xfd, 0xb8, 0x13, /* yellow */
         0xcb, 0x4b, 0x16, /* orange */
-        0xdc, 0x32, 0x2f, /* red */
+        0xef, 0x50, 0x26, /* red */
         0xd3, 0x36, 0x82, /* magenta */
         0x6c, 0x71, 0xc4, /* violet */
-        0x26, 0x8b, 0xd2, /* blue */
+        0x23, 0x99, 0xd7, /* blue */
         0x2a, 0xa1, 0x98, /* cyan */
-        0x85, 0x99, 0x00, /* green */
-        // 0x00, 0x00, 0x00,    /*  0:黒 */
-        // 0xff, 0x00, 0x00,    /*  1:明るい赤 */
-        // 0x00, 0xff, 0x00,    /*  2:明るい緑 */
-        // 0xff, 0xff, 0x00,    /*  3:明るい黄色 */
-        // 0x00, 0x00, 0xff,    /*  4:明るい青 */
-        // 0xff, 0x00, 0xff,    /*  5:明るい紫 */
-        // 0x00, 0xff, 0xff,    /*  6:明るい水色 */
-        // 0xff, 0xff, 0xff,    /*  7:白 */
-        // 0xc6, 0xc6, 0xc6,    /*  8:明るい灰色 */
-        // 0x84, 0x00, 0x00,    /*  9:暗い赤 */
-        // 0x00, 0x84, 0x00,    /* 10:暗い緑 */
-        // 0x84, 0x84, 0x00,    /* 11:暗い黄色 */
-        // 0x00, 0x00, 0x84,    /* 12:暗い青 */
-        // 0x84, 0x00, 0x84,    /* 13:暗い紫 */
-        // 0x00, 0x84, 0x84,    /* 14:暗い水色 */
-        // 0x84, 0x84, 0x84     /* 15:暗い灰色 */
+        0x7f, 0xbc, 0x43, /* green */
     };
     set_palette(0, 15, table_rgb);
     return;
@@ -120,13 +129,22 @@ void set_palette(int start, int end, unsigned char *rgb)
     return;
 }
 
-void boxfill8(unsigned char *vram, int xsize, unsigned char c,
+void boxfill8(unsigned char *vram, int X, unsigned char c,
               int x0, int y0, int x1, int y1)
 {
     int x, y;
     for (y = y0; y <= y1; y++) {
         for (x = x0; x <= x1; x++)
-            vram[y * xsize + x] = c;
+            vram[y * X + x] = c;
     }
+    return;
+}
+
+void boxsize8(unsigned char *vram, int X, unsigned char c,
+              int x0, int y0, int width, int height)
+{
+    int y1 = y0 + height - 1;
+    int x1 = x0 + width - 1;
+    boxfill8(vram, X, c, x0, y0, x1, y1);
     return;
 }
