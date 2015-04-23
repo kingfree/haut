12592e623674e92ca89520ac950e5adc8d154cab
Kingfree <anytjf@gmail.com>
2015-04-23 20:02:13 +0800

    DAY04 指针的语法糖

--- osask/src/bootpack.c
+++ osask/src/bootpack.c
@@ -5,10 +5,10 @@ void HariMain(void)
     int i; /* 声明变量 */
     char *p; /* 指向BYTE */
 
-    for (i = 0xa0000; i <= 0xaffff; i++) {
-        p = (char *) i;
-        *p = i & 0x0f;
-        /* 相当于 write_mem8(i, i & 0x0f); */
+    p = (char *) 0xa0000;
+
+    for (i = 0; i <= 0xffff; i++) {
+        p[i] = i & 0x0f;
     }
 
     for (; ; ) {
