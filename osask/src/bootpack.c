b77fb0825ea657ad0ec65fd49a76764b30b3ae1a
Kingfree <anytjf@gmail.com>
2015-04-24 09:50:51 +0800

    DAY05 显示字符串

--- osask/src/bootpack.c
+++ osask/src/bootpack.c
@@ -12,6 +12,7 @@ void boxsize8(unsigned char *vram, int X, unsigned char c,
               int x0, int y0, int width, int height);
 void init_screen(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
 
 #define base03   0
 #define base02   1
@@ -61,12 +62,9 @@ void HariMain(void)
 
     init_palette();
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-    putfont8(binfo->vram, binfo->scrnx,  8, 8, COL8_FFFFFF, hankaku + 'A' * 16);
-    putfont8(binfo->vram, binfo->scrnx, 16, 8, COL8_FFFFFF, hankaku + 'B' * 16);
-    putfont8(binfo->vram, binfo->scrnx, 24, 8, COL8_FFFFFF, hankaku + 'C' * 16);
-    putfont8(binfo->vram, binfo->scrnx, 40, 8, COL8_FFFFFF, hankaku + '1' * 16);
-    putfont8(binfo->vram, binfo->scrnx, 48, 8, COL8_FFFFFF, hankaku + '2' * 16);
-    putfont8(binfo->vram, binfo->scrnx, 56, 8, COL8_FFFFFF, hankaku + '3' * 16);
+    putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8, COL8_FFFFFF, "ABC 123");
+    putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS.");
+    putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS.");
 
     for (; ; ) {
         io_hlt();
@@ -183,6 +181,16 @@ void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
         if ((d & 0x04) != 0) { p[5] = c; }
         if ((d & 0x02) != 0) { p[6] = c; }
         if ((d & 0x01) != 0) { p[7] = c; }
+    }
+    return;
+}
+
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s)
+{
+    extern char hankaku[4096];
+    for (; *s != 0x00; s++) {
+        putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
+        x += 8;
     }
     return;
 }
