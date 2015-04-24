5e2d504edeac010426213eb59fc8fab614156935
Kingfree <anytjf@gmail.com>
2015-04-24 09:54:35 +0800

    DAY05 使用sprintf打印值

--- osask/src/bootpack.c
+++ osask/src/bootpack.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void io_hlt(void);
 void io_cli(void);
 void io_out8(int port, int data);
@@ -58,13 +60,15 @@ typedef struct BOOTINFO {
 void HariMain(void)
 {
     bootinfo_t *binfo = (bootinfo_t *) 0x0ff0;
-    extern char hankaku[4096];
+	char s[40];
 
     init_palette();
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
     putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8, COL8_FFFFFF, "ABC 123");
     putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS.");
     putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS.");
+	sprintf(s, "scrnx = %d, scrny = %d", binfo->scrnx, binfo->scrny);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 
     for (; ; ) {
         io_hlt();
