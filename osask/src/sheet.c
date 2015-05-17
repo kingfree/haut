af134ad12747211077c83327d8f3313b8c3df0b2
Kingfree <anytjf@gmail.com>
2015-05-17 16:03:55 +0800

    DAY26 一次写入4字节加快移动

--- osask/src/sheet.c
+++ osask/src/sheet.c
@@ -54,9 +54,9 @@ void sheet_setbuf(sheet_t *sht, unsigned char *buf, int xsize, int ysize, int al
     return;
 }
 
-void sheet_refreshmap(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0)
+void sheet_refreshmap(shtctl_t *ctl, int vx0, int vy0, int vx1, int vy1, int h0)
 {
-    int h, bx, by, vx, vy, bx0, by0, bx1, by1;
+    int h, bx, by, vx, vy, bx0, by0, bx1, by1, sid4, *p;
     unsigned char *buf, sid, *map = ctl->map;
     sheet_t *sht;
     if (vx0 < 0) { vx0 = 0; }
@@ -76,12 +76,26 @@ void sheet_refreshmap(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, in
         if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
         if (by1 > sht->bysize) { by1 = sht->bysize; }
         if (sht->alpha == -1) {
-            /* 无透明色专用的高速版 */
-            for (by = by0; by < by1; by++) {
-                vy = sht->vy0 + by;
-                for (bx = bx0; bx < bx1; bx++) {
-                    vx = sht->vx0 + bx;
-                    map[vy * ctl->xsize + vx] = sid;
+            if ((sht->vx0 & 3) == 0 && (bx0 & 3) == 0 && (bx1 & 3) == 0) {
+                /* 无透明色专用的高速版（4字节型） */
+                bx1 = (bx1 - bx0) / 4; /* MOV次数 */
+                sid4 = sid | sid << 8 | sid << 16 | sid << 24;
+                for (by = by0; by < by1; by++) {
+                    vy = sht->vy0 + by;
+                    vx = sht->vx0 + bx0;
+                    p = (int *) &map[vy * ctl->xsize + vx];
+                    for (bx = 0; bx < bx1; bx++) {
+                        p[bx] = sid4;
+                    }
+                }
+            } else {
+                /* 无透明色专用的高速版（1字节型） */
+                for (by = by0; by < by1; by++) {
+                    vy = sht->vy0 + by;
+                    for (bx = bx0; bx < bx1; bx++) {
+                        vx = sht->vx0 + bx;
+                        map[vy * ctl->xsize + vx] = sid;
+                    }
                 }
             }
         } else {
@@ -102,7 +116,7 @@ void sheet_refreshmap(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, in
 
 void sheet_refreshsub(shtctl_t *ctl, int vx0, int vy0, int vx1, int vy1, int h0, int h1)
 {
-    int h, bx, by, vx, vy, bx0, by0, bx1, by1;
+    int h, bx, by, vx, vy, bx0, by0, bx1, by1, bx2, sid4, i, i1, *p, *q, *r;
     unsigned char *buf, *vram = ctl->vram, *map = ctl->map, sid;
     sheet_t *sht;
     /* 修正超出屏幕的刷新范围 */
@@ -123,12 +137,60 @@ void sheet_refreshsub(shtctl_t *ctl, int vx0, int vy0, int vx1, int vy1, int h0,
         if (by0 < 0) { by0 = 0; }
         if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
         if (by1 > sht->bysize) { by1 = sht->bysize; }
-        for (by = by0; by < by1; by++) {
-            vy = sht->vy0 + by;
-            for (bx = bx0; bx < bx1; bx++) {
+        if ((sht->vx0 & 3) == 0) {
+            /* 4字节型 */
+            i  = (bx0 + 3) / 4; /* bx0除以4（上取整） */
+            i1 =  bx1      / 4; /* bx1除以4（下取整） */
+            i1 = i1 - i;
+            sid4 = sid | sid << 8 | sid << 16 | sid << 24;
+            for (by = by0; by < by1; by++) {
+                vy = sht->vy0 + by;
+                for (bx = bx0; bx < bx1 && (bx & 3) != 0; bx++) {   /* 前面被4除余下部分逐字节写入 */
+                    vx = sht->vx0 + bx;
+                    if (map[vy * ctl->xsize + vx] == sid) {
+                        vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+                    }
+                }
                 vx = sht->vx0 + bx;
-                if (map[vy * ctl->xsize + vx] == sid) {
-                    vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+                p = (int *) &map[vy * ctl->xsize + vx];
+                q = (int *) &vram[vy * ctl->xsize + vx];
+                r = (int *) &buf[by * sht->bxsize + bx];
+                for (i = 0; i < i1; i++) {                          /* 4的倍数部分 */
+                    if (p[i] == sid4) {
+                        q[i] = r[i];
+                    } else {
+                        bx2 = bx + i * 4;
+                        vx = sht->vx0 + bx2;
+                        if (map[vy * ctl->xsize + vx + 0] == sid) {
+                            vram[vy * ctl->xsize + vx + 0] = buf[by * sht->bxsize + bx2 + 0];
+                        }
+                        if (map[vy * ctl->xsize + vx + 1] == sid) {
+                            vram[vy * ctl->xsize + vx + 1] = buf[by * sht->bxsize + bx2 + 1];
+                        }
+                        if (map[vy * ctl->xsize + vx + 2] == sid) {
+                            vram[vy * ctl->xsize + vx + 2] = buf[by * sht->bxsize + bx2 + 2];
+                        }
+                        if (map[vy * ctl->xsize + vx + 3] == sid) {
+                            vram[vy * ctl->xsize + vx + 3] = buf[by * sht->bxsize + bx2 + 3];
+                        }
+                    }
+                }
+                for (bx += i1 * 4; bx < bx1; bx++) {                /* 余下部分逐字节写入 */
+                    vx = sht->vx0 + bx;
+                    if (map[vy * ctl->xsize + vx] == sid) {
+                        vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+                    }
+                }
+            }
+        } else {
+            /* 1字节型 */
+            for (by = by0; by < by1; by++) {
+                vy = sht->vy0 + by;
+                for (bx = bx0; bx < bx1; bx++) {
+                    vx = sht->vx0 + bx;
+                    if (map[vy * ctl->xsize + vx] == sid) {
+                        vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+                    }
                 }
             }
         }
