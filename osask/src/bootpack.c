e0ec29948aaafe69d4e6de893c2acc93c1566de9
Kingfree <anytjf@gmail.com>
2015-05-19 17:30:26 +0800

    DAY26 Ctrl+T启动终端

--- osask/src/bootpack.c
+++ osask/src/bootpack.c
@@ -8,6 +8,7 @@
 
 void keywin_off(sheet_t *key_win);
 void keywin_on(sheet_t *key_win);
+sheet_t *open_console(shtctl_t *shtctl, unsigned int memtotal);
 
 void HariMain(void)
 {
@@ -78,29 +79,8 @@ void HariMain(void)
     init_screen8(buf_back, binfo->scrnx, binfo->scrny);
 
     /* sht_cons */
-    for (i = 0; i < 2; i++) {
-        sht_cons[i] = sheet_alloc(shtctl);
-        buf_cons[i] = (unsigned char *) memman_alloc_4k(memman, CONS_WINW * CONS_WINH);
-        sheet_setbuf(sht_cons[i], buf_cons[i], CONS_WINW, CONS_WINH, -1); /* 无透明色 */
-        make_window8(buf_cons[i], CONS_WINW, CONS_WINH, "Terminal", 0);
-        make_textbox8(sht_cons[i], CONS_LEFT, CONS_TOP, CONS_COLW, CONS_LINH, base03);
-        task_cons[i] = task_alloc();
-        task_cons[i]->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 12;
-        task_cons[i]->tss.eip = (int) &console_task;
-        task_cons[i]->tss.es = 1 * 8;
-        task_cons[i]->tss.cs = 2 * 8;
-        task_cons[i]->tss.ss = 1 * 8;
-        task_cons[i]->tss.ds = 1 * 8;
-        task_cons[i]->tss.fs = 1 * 8;
-        task_cons[i]->tss.gs = 1 * 8;
-        *((int *) (task_cons[i]->tss.esp + 4)) = (int) sht_cons[i];
-        *((int *) (task_cons[i]->tss.esp + 8)) = memtotal;
-        task_run(task_cons[i], 2, 2); /* level=2, priority=2 */
-        sht_cons[i]->task = task_cons[i];
-        sht_cons[i]->flags |= 0x20; /* 由光标 */
-        cons_fifo[i] = (int *) memman_alloc_4k(memman, 128 * 4);
-        fifo32_init(&task_cons[i]->fifo, 128, cons_fifo[i], task_cons[i]);
-    }
+    sht_cons[0] = open_console(shtctl, memtotal);
+    sht_cons[1] = 0; /* 未打开状态 */
 
     /* sht_mouse */
     sht_mouse = sheet_alloc(shtctl);
@@ -110,13 +90,11 @@ void HariMain(void)
     int my = (binfo->scrny - CURSOR_Y) / 2;
 
     sheet_slide(sht_back, 0, 0);
-    sheet_slide(sht_cons[1], 8, 2);
-    sheet_slide(sht_cons[0], 120, 60);
+    sheet_slide(sht_cons[0], 32, 4);
     sheet_slide(sht_mouse, mx, my);
     sheet_updown(sht_back, 0);
-    sheet_updown(sht_cons[1], 1);
-    sheet_updown(sht_cons[0], 2);
-    sheet_updown(sht_mouse, 3);
+    sheet_updown(sht_cons[0], 1);
+    sheet_updown(sht_mouse, 2);
     key_win = sht_cons[0];
     keywin_on(key_win);
 
@@ -227,6 +205,15 @@ void HariMain(void)
                         io_sti();
                     }
                 }
+                if (i == 256 + 0x14 && key_ctrl != 0) { /* Ctrl + T */
+                    sht_cons[1] = open_console(shtctl, memtotal);
+                    sheet_slide(sht_cons[1], 100, 80);
+                    sheet_updown(sht_cons[1], shtctl->top);
+                    /* 将输入焦点切换到新打开的命令行窗口 */
+                    keywin_off(key_win);
+                    key_win = sht_cons[1];
+                    keywin_on(key_win);
+                }
                 if (i == 256 + 0x57 && shtctl->top > 2) {   /* F11 */
                     sheet_updown(shtctl->sheets[1], shtctl->top - 1);
                 }
@@ -332,3 +319,31 @@ void keywin_on(sheet_t *key_win)
     }
     return;
 }
+
+sheet_t *open_console(shtctl_t *shtctl, unsigned int memtotal)
+{
+    memman_t *memman = (memman_t *) MEMMAN_ADDR;
+    sheet_t *sht = sheet_alloc(shtctl);
+    unsigned char *buf = (unsigned char *) memman_alloc_4k(memman, CONS_WINW * CONS_WINH);
+    task_t *task = task_alloc();
+    int *cons_fifo = (int *) memman_alloc_4k(memman, 128 * 4);
+    sheet_setbuf(sht, buf, CONS_WINW, CONS_WINH, -1); /* 无透明色 */
+    make_window8(buf, CONS_WINW, CONS_WINH, "Terminal", 0);
+    make_textbox8(sht, CONS_LEFT, CONS_TOP, CONS_COLW, CONS_LINH, base03);
+    task->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 12;
+    task->tss.eip = (int) &console_task;
+    task->tss.es = 1 * 8;
+    task->tss.cs = 2 * 8;
+    task->tss.ss = 1 * 8;
+    task->tss.ds = 1 * 8;
+    task->tss.fs = 1 * 8;
+    task->tss.gs = 1 * 8;
+    *((int *) (task->tss.esp + 4)) = (int) sht;
+    *((int *) (task->tss.esp + 8)) = memtotal;
+    task_run(task, 2, 2); /* level=2, priority=2 */
+    sht->task = task;
+    sht->flags |= 0x20; /* 有光标 */
+    fifo32_init(&task->fifo, 128, cons_fifo, task);
+    return sht;
+}
+
