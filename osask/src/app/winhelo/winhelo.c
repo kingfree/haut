a5658be1c17f02afcec6275103ad156b8ce25ad5
Kingfree <anytjf@gmail.com>
2015-05-24 21:20:22 +0800

    DAY28 局部大变量

--- osask/src/app/winhelo/winhelo.c
+++ osask/src/app/winhelo/winhelo.c
@@ -1,11 +1,10 @@
 #include "apilib.h"
 
-char buf[240 * 80];
-
 void HariMain(void)
 {
     int win;
-    win = api_openwin(buf, 240, 80, -1, "hello");
+	char buf[150 * 50];
+    win = api_openwin(buf, 150, 50, -1, "hello");
     for (;;) {
         if (api_getkey(1) == 0x0a) {
             break;
