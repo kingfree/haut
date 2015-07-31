172e824f3ec507f1870c609d9acc5ae69833307e
Kingfree <anytjf@gmail.com>
2015-07-31 10:17:12 +0800

    解除 OS X 下的编译警告

--- inlonlife/openssl/md5.c
+++ inlonlife/openssl/md5.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-#include <openssl/md5.h>
+
+#if defined(__APPLE__)
+#  define COMMON_DIGEST_FOR_OPENSSL
+#  include <CommonCrypto/CommonDigest.h>
+#else
+#  include <openssl/md5.h>
+#endif
+
+#define BUFFSIZE 1024
 
 int main(int argc, char *argv[])
 {
@@ -7,26 +15,26 @@ int main(int argc, char *argv[])
     char *filename = "md5.c";
     if (argc > 1) filename = argv[1];
 
-    FILE *inFile = fopen(filename, "rb");
-    MD5_CTX mdContext;
+    FILE *file = fopen(filename, "rb");
+    MD5_CTX md5;
     int bytes;
-    unsigned char data[1024];
+    unsigned char data[BUFFSIZE];
 
-    if (inFile == NULL) {
+    if (file == NULL) {
         printf("%s can't be opened.\n", filename);
         return 0;
     }
 
-    MD5_Init(&mdContext);
-    while ((bytes = fread(data, 1, 1024, inFile)) != 0)
-        MD5_Update(&mdContext, data, bytes);
-    MD5_Final(c, &mdContext);
+    MD5_Init(&md5);
+    while ((bytes = fread(data, 1, BUFFSIZE, file)) != 0)
+        MD5_Update(&md5, data, bytes);
+    MD5_Final(c, &md5);
 
     int i;
     for (i = 0; i < MD5_DIGEST_LENGTH; i++) printf("%02x", c[i]);
     printf(" %s\n", filename);
 
-    fclose(inFile);
+    fclose(file);
 
     return 0;
 }
