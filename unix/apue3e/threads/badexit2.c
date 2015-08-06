ea5306d9670b308a41c45a0da7fd7c08e97552e1
Kingfree <anytjf@gmail.com>
2015-08-06 10:55:13 +0800

    线程终止

--- unix/apue3e/threads/badexit2.c
+++ unix/apue3e/threads/badexit2.c
@@ -5,48 +5,45 @@ struct foo {
     int a, b, c, d;
 };
 
-void printfoo(const char* s, const struct foo* fp)
+void printfoo(const char *s, const struct foo *fp)
 {
     printf("%s", s);
-    printf("  structure at 0x%lx\n", (unsigned long)fp);
+    printf("  结构体 0x%lx\n", (unsigned long)fp);
     printf("  foo.a = %d\n", fp->a);
     printf("  foo.b = %d\n", fp->b);
     printf("  foo.c = %d\n", fp->c);
     printf("  foo.d = %d\n", fp->d);
 }
 
-void* thr_fn1(void* arg)
+void *thr_fn1(void *arg)
 {
-    struct foo foo = { 1, 2, 3, 4 };
+    struct foo foo = {1, 2, 3, 4};
 
-    printfoo("thread 1:\n", &foo);
-    pthread_exit((void*)&foo);
+    printfoo("线程 1:\n", &foo);
+    pthread_exit((void *)&foo);
 }
 
-void* thr_fn2(void* arg)
+void *thr_fn2(void *arg)
 {
-    printf("thread 2: ID is %lu\n", (unsigned long)pthread_self());
-    pthread_exit((void*)0);
+    printf("线程 2: ID %lu\n", (unsigned long)pthread_self());
+    pthread_exit((void *)0);
 }
 
 int main(void)
 {
     int err;
     pthread_t tid1, tid2;
-    struct foo* fp;
+    struct foo *fp;
 
     err = pthread_create(&tid1, NULL, thr_fn1, NULL);
-    if (err != 0)
-        err_exit(err, "can't create thread 1");
-    err = pthread_join(tid1, (void*)&fp);
-    if (err != 0)
-        err_exit(err, "can't join with thread 1");
+    if (err != 0) err_exit(err, "无法创建线程1");
+    err = pthread_join(tid1, (void *)&fp);
+    if (err != 0) err_exit(err, "无法同步线程1");
     sleep(1);
-    printf("parent starting second thread\n");
+    printf("父线程启动第二个线程\n");
     err = pthread_create(&tid2, NULL, thr_fn2, NULL);
-    if (err != 0)
-        err_exit(err, "can't create thread 2");
+    if (err != 0) err_exit(err, "无法创建线程2");
     sleep(1);
-    printfoo("parent:\n", fp);
+    printfoo("父线程:\n", fp);
     exit(0);
 }
