35b8317bfd54732baf734cd37de92dff27730f8b
SakiKoromoAko <anytjf@live.com>
2015-08-07 17:23:07 +0900

    多线程排序

--- unix/apue3e/threads/barrier.c
+++ unix/apue3e/threads/barrier.c
@@ -3,9 +3,9 @@
 #include <limits.h>
 #include <sys/time.h>
 
-#define NTHR 8               /* number of threads */
-#define NUMNUM 8000000L      /* number of numbers to sort */
-#define TNUM (NUMNUM / NTHR) /* number to sort per thread */
+#define NTHR 8               /* 线程数 */
+#define NUMNUM 8000000L      /* 待排序数量 */
+#define TNUM (NUMNUM / NTHR) /* 每个线程排序数量 */
 
 long nums[NUMNUM];
 long snums[NUMNUM];
@@ -16,11 +16,11 @@ pthread_barrier_t b;
 #define heapsort qsort
 #else
 extern int heapsort(void *, size_t, size_t,
-                    int (*)(const void *, const void *));
+        int (*)(const void *, const void *));
 #endif
 
 /*
- * Compare two long integers (helper function for heapsort)
+ * 比较两个长整型数
  */
 int complong(const void *arg1, const void *arg2)
 {
@@ -36,7 +36,7 @@ int complong(const void *arg1, const void *arg2)
 }
 
 /*
- * Worker thread to sort a portion of the set of numbers.
+ * 部分排序线程
  */
 void *thr_fn(void *arg)
 {
@@ -46,13 +46,13 @@ void *thr_fn(void *arg)
     pthread_barrier_wait(&b);
 
     /*
-         * Go off and perform more work ...
-         */
+     * 执行其他操作
+     */
     return ((void *)0);
 }
 
 /*
- * Merge the results of the individual sorted ranges.
+ * 合并排序结果
  */
 void merge()
 {
@@ -83,31 +83,31 @@ int main()
     pthread_t tid;
 
     /*
-         * Create the initial set of numbers to sort.
-         */
+     * 初始化待排序数组
+     */
     srandom(1);
     for (i = 0; i < NUMNUM; i++) nums[i] = random();
 
     /*
-         * Create 8 threads to sort the numbers.
-         */
+     * 创建 8 个用于排序的线程
+     */
     gettimeofday(&start, NULL);
     pthread_barrier_init(&b, NULL, NTHR + 1);
     for (i = 0; i < NTHR; i++) {
         err = pthread_create(&tid, NULL, thr_fn, (void *)(i * TNUM));
-        if (err != 0) err_exit(err, "can't create thread");
+        if (err != 0) err_exit(err, "无法创建线程");
     }
     pthread_barrier_wait(&b);
     merge();
     gettimeofday(&end, NULL);
 
     /*
-         * Print the sorted list.
-         */
+     * 打印排序结果
+     */
     startusec = start.tv_sec * 1000000 + start.tv_usec;
     endusec = end.tv_sec * 1000000 + end.tv_usec;
     elapsed = (double)(endusec - startusec) / 1000000.0;
-    printf("sort took %.4f seconds\n", elapsed);
+    printf("排序用时 %.4f 秒\n", elapsed);
     for (i = 0; i < NUMNUM; i++) printf("%ld\n", snums[i]);
     exit(0);
 }
